3b3a0548e489b3a7abc9c5840b776acf80e8c847
Gaston Jauregui <gaston.jaureguib@gmail.com>
2026-07-14 17:24:36 -0300

    actualizando la planilla de variables

--- src/app.js
+++ src/app.js
@@ -2,18 +2,18 @@ import { ServiceManager } from './managers/ServiceManager.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// 1. Preparamos la ruta del archivo
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const rutaDeDatos = path.join(__dirname, './data/services.json');
 
-// 2. Instanciamos el manager (AQUÍ ESTÁ LA VARIABLE QUE TE FALTABA)
+
 const manager = new ServiceManager(rutaDeDatos);
 
-// 3. Ejecutamos nuestra prueba
+
 const test = async () => {
     console.log("Agregando servicio de viaje...");
     
-    // Ahora 'manager' ya existe aquí porque está en el mismo archivo
+   
     await manager.addService({ 
         name: "Tour por Europa", 
         description: "Recorrido por París, Roma y Madrid", 
